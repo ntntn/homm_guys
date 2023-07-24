c98357e62f432ceca39d68c1294b512f1881ca79
a.soldatenkov@hezzl.com <a.soldatenkov@hezzl.com>
2023-07-24 17:33:49 +0300

    add earth chubrik

--- src/scripts/scenes/GameScene.ts
+++ src/scripts/scenes/GameScene.ts
@@ -1,14 +1,22 @@
 import { Main } from "../Assets";
 import { CustomScene } from "../CustomScene";
 
+const wrap = (num: number, min: number, max: number): number => ((((num - min) % (max - min)) + (max - min)) % (max - min)) + min;
+(window as any).wrap = wrap;
+
 export default class GameScene extends CustomScene
 {
 	static instance: GameScene;
 	cols: number;
 	rows: number;
 	cellSize: number;
-	worldWidth: number;
-	worldHeight: number;
+	width: number;
+	height: number;
+
+	currentChubrik: Phaser.GameObjects.Sprite; // ТЕКУЩИЙ ЧУБРИК (ТОТ, ЧЕЙ ХОД)
+	waterChubrik: Phaser.GameObjects.Sprite;
+	fireChubrik: Phaser.GameObjects.Sprite;
+	earthChubrik: any;
 
 	constructor()
 	{
@@ -19,59 +27,138 @@ export default class GameScene extends CustomScene
 
 	create()
 	{
-		this.createGrid();
-		this.createChubrik();
-		this.add.rectangle(0, 0, this.worldWidth, this.worldHeight, 0xff0000, 0.25);
+		this.createGrid(); //создать поле
+		this.createFireChubrik(); //создать огненного чубрика
+		this.createWaterChubrik(); //создать водяного чубрика
+		this.createEarthChubrik(); //создать земляного чубрика
+		this.input.on("pointerdown", this.onClick, this); //отлавливание клика мышки
 
-		this.show();
-		this.initResize();
+		this.initResize(); //неважно
 	}
 
 	createGrid()
 	{
-		this.cols = 10;
-		this.rows = 10;
-		this.cellSize = 32;
-		this.worldWidth = this.cols * this.cellSize;
-		this.worldHeight = this.rows * this.cellSize;
+		this.cols = 10; //столбцы
+		this.rows = 10; //строчки
+		this.cellSize = 32; //размер одной клетки в пикселях
+		this.width = this.cols * this.cellSize; //ширина доски в пикселях
+		this.height = this.rows * this.cellSize; //высота доски в пикселях
 
-		for (let row = 0; row < this.rows; row++)
+		for (let row = 0; row < this.rows; row++) //по каждой строчке
 		{
-			for (let col = 0; col < this.cols; col++)
+			for (let col = 0; col < this.cols; col++) //по каждому столбцу
 			{
-				const wp = this.getWorldPosition(col, row);
-				const rect = this.add.rectangle(wp.x, wp.y, this.cellSize, this.cellSize, 0x00ff00, 0.25);
-				rect.setStrokeStyle(2, 0x000000, 1);
-				rect.isStroked = true;
+				const pos = this.getWorldPosition(col, row); //получаем позицию в пикселях по столбцу и строке
+				const rect = this.add.rectangle(pos.x, pos.y, this.cellSize, this.cellSize, 0x00ff00, 0.25); //создаем прямоугольник в полученных координатах
+				rect.setStrokeStyle(2, 0x000000, 1); //неважно
+				rect.isStroked = true; //неважно
 			}
 		}
 	}
 
-	createChubrik(col = 0, row = 0)
+	createFireChubrik(col = 0, row = 0)
 	{
-		const wp = this.getWorldPosition(col, row);
-		const chubrik = this.add.sprite(wp.x, wp.y, Main.Key, Main.chubrik1).setScale(0.15);
-
 		this.anims.create({
-			key: 'chubrik',
+			key: 'fire_chubrik',
 			frames: this.anims.generateFrameNames(Main.Key, {
 				start: 1,
 				end: 4,
-				prefix: "chubrik1_"
+				prefix: "fire_chubrik (",
+				suffix: ")"
 			}),
 			skipMissedFrames: true,
 			repeat: -1,
 			frameRate: 6
 		});
 
-		chubrik.play("chubrik");
+		const pos = this.getWorldPosition(col, row);
+		this.fireChubrik = this.add.sprite(pos.x, pos.y, Main.Key, Main.chubrik1)
+			.setScale(0.15)
+			.play("fire_chubrik");
+
+		this.currentChubrik = this.fireChubrik; //задаем чубрика чей ход
 	}
+
+	createWaterChubrik(col = 9, row = 9)
+	{
+		const wp = this.getWorldPosition(col, row);
+		this.waterChubrik = this.add.sprite(wp.x, wp.y, Main.Key, Main["water_chubrik (1)"]).setScale(0.2);
+
+		this.anims.create({
+			key: 'water_chubrik',
+			frames: this.anims.generateFrameNames(Main.Key, {
+				start: 1,
+				end: 4,
+				prefix: "water_chubrik (",
+				suffix: ")"
+			}),
+			skipMissedFrames: true,
+			repeat: -1,
+			frameRate: 6
+		});
+
+		this.waterChubrik.play("water_chubrik");
+	}
+
+	createEarthChubrik(col = 5, row = 5)
+	{
+		const wp = this.getWorldPosition(col, row);
+		this.earthChubrik = this.add.sprite(wp.x, wp.y, Main.Key, Main["earth_chubrik (1)"]).setScale(0.2);
+
+		this.anims.create({
+			key: 'earth_chubrik',
+			frames: this.anims.generateFrameNames(Main.Key, {
+				start: 1,
+				end: 4,
+				prefix: "earth_chubrik (",
+				suffix: ")"
+			}),
+			skipMissedFrames: true,
+			repeat: -1,
+			frameRate: 6
+		});
+
+		this.earthChubrik.play("earth_chubrik");
+	}
+
+	onClick(ptr: Phaser.Input.Pointer)
+	{
+		const x = ptr.worldX;
+		const y = ptr.worldY;
+		const logicPos = this.getLogicPosition(x, y);
+		const pos = this.getWorldPosition(logicPos.col, logicPos.row);
+
+		this.tweens.add({
+			targets: this.currentChubrik,
+			x: pos.x,
+			y: pos.y,
+			duration: 500,
+			onComplete: () =>
+			{
+				const chubriks = [this.fireChubrik, this.waterChubrik, this.earthChubrik]; //все чубрики
+				const index = chubriks.indexOf(this.currentChubrik); //индекс текущего чубрика
+				const nextIndex = wrap(index + 1, 0, chubriks.length); //инндекс следующего чубрика
+				console.log(index, nextIndex);
+				this.currentChubrik = chubriks[nextIndex]; //изменяем текущего чубрика на следующего
+			}
+		});
+	}
+
+
 
 	getWorldPosition(col: number, row: number)
 	{
 		return {
-			x: col * this.cellSize - this.worldWidth * .5 + this.cellSize * .5,
-			y: row * this.cellSize - this.worldWidth * .5 + this.cellSize * .5
+			x: col * this.cellSize - this.width * .5 + this.cellSize * .5,
+			y: row * this.cellSize - this.width * .5 + this.cellSize * .5
+		}
+	}
+
+	getLogicPosition(x: number, y: number)
+	{
+		return {
+			col: Math.floor((x + this.width * .5) / this.cellSize),
+			row: Math.floor((y + this.width * .5) / this.cellSize)
 		}
 	}
 
